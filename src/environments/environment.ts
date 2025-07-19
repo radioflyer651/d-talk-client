fc52d9001cbfea0e8b5259d01ea5e1178ab14a37
Richard Olson <richard@richardolson.org>
2025-07-19 09:46:51 -0500

    fix: update API base URL and socket endpoint for chat service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { AppConfig } from "../model/app-config.models";
 
 export const environment: AppConfig = {
     production: true,
-    apiBaseUrl: 'https://www.richardolson.org/api/',
-    chatSocketIoEndpoint: 'https://www.richardolson.org',
+    apiBaseUrl: 'https://chat.richardolson.org/api/',
+    chatSocketIoEndpoint: 'https://chat.richardolson.org',
     chatSocketPath: '/chat-io/'
 };
